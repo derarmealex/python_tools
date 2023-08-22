5c049c54a70e78c1f2390037e6bce53c529c01a4
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-22 17:30:37 +0200

    Update input_while_set_words.py

--- input_while_set_words.py
+++ input_while_set_words.py
@@ -8,3 +8,11 @@ while len(words) < 3:
     else:
         print("The word is not four character".upper())
 print("Three words are already saved:\n", ", ".join(words))
+# or
+words = set()
+while len(words) < 3:
+    word = input("Enter a four character word: ")
+    if len(word) == 4 and word not in words: words.add(word)
+    elif word in words: print("Word", word.upper(), "is already entered")
+    else: print("The word is not four character".upper())
+print("Three words are already saved:\n", ", ".join(words))
