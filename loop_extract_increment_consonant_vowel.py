025e03ae16be8b0d6aaba4cb8daf483a334534d8
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-22 17:12:39 +0200

    Update loop_extract_increment_consonant_vowel.py

--- loop_extract_increment_consonant_vowel.py
+++ loop_extract_increment_consonant_vowel.py
@@ -13,3 +13,9 @@ for letter in sentence:
         
 #print("Consonants:", final["Consonants"], "|", "Vowels:", final["Vowels"])
 print(" | ".join([f"{key}: {value}" for key, value in final.items()]))
+# BOOL
+def fc(z):
+    x = 'eyuioa'
+#    print([f"{z} - {z in x}" for z in z if z in x])                       # ['u - True', 'e - True', 'i - True', ...]
+    print([f"{z} - {z in x}" if z in x else f"{z} - {z in x}" for z in z]) # ['Z - False', 'v - False', 'u - True', ...]
+fc('Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu')
