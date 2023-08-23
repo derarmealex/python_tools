28b638f8567a9f105b4ff1bed4ef718c85e9d9aa
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-23 17:23:40 +0200

    Update loop_extract_consonant_vowel.py

--- loop_extract_consonant_vowel.py
+++ loop_extract_consonant_vowel.py
@@ -19,3 +19,32 @@ def fc(z):
 #    print([f"{z} - {z in x}" for z in z if z in x])                       # ['u - True', 'e - True', 'i - True', ...]
     print([f"{z} - {z in x}" if z in x else f"{z} - {z in x}" for z in z]) # ['Z - False', 'v - False', 'u - True', ...]
 fc('Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu')
+# GENERATOR
+z = 'Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu'
+x = 'eyuioa'
+w = (f"{z} - {z in x}" if z in x else f"{z} - {z in x}" for z in z)
+print(next(w))                                                             # Z - False
+print(next(w))                                                             # v - False
+print(next(w))                                                             # u - True
+print(next(w))                                                             # k - False
+# or
+z = 'Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu'
+x = 'eyuioa'
+def fung():
+    for z in z:
+        yield z
+gen = (f"{z} - {z in x}" if z in x else f"{z} - {z in x}" for z in z)
+print(next(gen))                                                           # Z - False
+print(next(gen))                                                           # v - False
+print(next(gen))                                                           # u - True
+# or
+z = 'Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu'
+x = 'eyuioa'
+def fung():
+    for z in z:
+        yield z
+gen = (f"{z} - {z in x}" if z in x else f"{z} - {z in x}" for z in z)
+itr = 0
+while itr < len(z):                                                        # Z - False
+    print(next(gen))                                                       # v - False
+    itr += 1                                                               # u - True ...
