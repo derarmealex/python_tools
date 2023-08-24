0f8a8bd5c1c2a289f8b4593586c5cc98f5d19de1
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-24 17:05:49 +0200

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,9 +1,9 @@
 def func(x, n):
     return x * n
-    
+
 print(func(2, 6))                               # 12
 print(func(3, 6))                               # 18
-#or
+# or
 x = 2
 n = 6
 print(func(x, n))                               # 12
@@ -11,16 +11,13 @@ print(func(x, n))                               # 12
 a = lambda x, n: x * n
 print(a(2, 6))                                  # 12
 print(a(3, 6))                                  # 18
-
+# or
 def func(n):
     return lambda x : x * n
-
 myd = func(2)
 myt = func(3)
 print(myd(6))                                   # 12
 print(myt(6))                                   # 18
-
+# or
 print(func(2)(6))                               # 12
 print(func(3)(6))                               # 18
-
-input()
