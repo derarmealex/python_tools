10364e7a4a2b0601b68ef981588a79f722983a20
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-27 13:48:10 +0200

    Update text_formatting.py

--- text_formatting.py
+++ text_formatting.py
@@ -6,10 +6,18 @@ print(
 'Twinkle, twinkle, little star,\n'
 '\tHow I wonder what you are'
 )
-#HEREDOC
+# HEREDOC
 print("""
 a string that you "don't" have to escape
 This
 is a ....... multi-line
 heredoc string --------> example
     """)
+# .FORMAT()
+name, age = "Simon", 19                                             # Name: Simon
+address = "Bangalore, Karnataka, India"                             ##Age: 19
+print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))  ##Address: Bangalore, Karnataka, India
+
+x, y = 4, 3
+result = x * x + 2 * x * y + y * y
+print("({} + {}) ^ 2) = {}".format(x, y, result))
