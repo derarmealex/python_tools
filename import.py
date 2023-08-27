f2c49b649ced97a73787b2a4fe79f5508d3708c5
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-08-27 14:37:39 +0200

    Update import.py

--- import.py
+++ import.py
@@ -11,14 +11,23 @@ print(pi)                                           # 3.141592653589793
 print(math.pi)                                      # 3.141592653589793
 
 import sys
-print(sys.version)
+print(sys.version)                                  # 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)]
+print(sys.platform)                                 # win32
+
+import os
+print(os.name)                                      # nt
 
 import platform
 print(platform.python_version())                    # 3.11.5
 print(platform.system())                            # Windows
+print(platform.release())                           # 10
+print(platform.machine())                           # AMD64
 print(platform.architecture())                      # ('64bit', 'WindowsPE')
 print(platform.architecture()[0])                   # 64bit
 
 import os.path
-print(os.path.isfile('Bob Kerrey-The Band Played Waltzing Matilda.mp3'))
-print(os.path.exists('Bob Kerrey-The Band Played Waltzing Matilda.mp3'))
+print(os.path.isfile('Bob Kerrey-The Band Played Waltzing Matilda.mp3')) # True/False
+print(os.path.exists('Bob Kerrey-The Band Played Waltzing Matilda.mp3')) #True/False
+
+import site
+print(site.getsitepackages())                       # ['C:\\Program Files\\WindowsApps\\PythonSoftwareFoundation.Python.3.11...
