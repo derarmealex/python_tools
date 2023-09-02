beab6b7ebeccf0f5ae6d8797bd9c09ef71951ff5
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-09-02 14:11:52 +0200

    Update import.py

--- import.py
+++ import.py
@@ -9,7 +9,8 @@ print(getpass.getuser())                       #
 import math
 #print(pi)                                     # NameError
 print(math.pi)                                 # 3.141592653589793
-print(math.modf(math.pi))                      # (0.14159265358979312, 3.0)
+print(int(math.modf(math.pi)[-1]))             # 3
+print(round(math.modf(math.pi)[0], 2))         # 0.14
 print(math.floor(4.5))                         # 4
 print(math.ceil(4.5))                          # 5
 
@@ -20,19 +21,27 @@ print(math.pi)                                 # 3.141592653589793
 import os
 print(os.name)                                 # nt
 print(os.cpu_count())                          # 4
+print(os.path.isabs('Bob Kerrey-The Band Played Waltzing Matilda.mp3'))  # True/False
 print(os.path.isfile('Bob Kerrey-The Band Played Waltzing Matilda.mp3')) # True/False
 print(os.path.isdir('Bob Kerrey-The Band Played Waltzing Matilda.mp3'))  # True/False
+print(os.path.islink('Bob Kerrey-The Band Played Waltzing Matilda.mp3')) # True/False
 print(os.path.exists('Bob Kerrey-The Band Played Waltzing Matilda.mp3')) # True/False
-print(os.path.abspath('LesDeuxTimides(1928)_movietroll.avi')) # C:\Users\alexa\Desktop\LesDeuxTimides(1928)_movietroll.avi
+print(os.path.lexists('Bob Kerrey-The Band Played Waltzing Matilda.mp3'))# True/False
+
+#C:\Users\alexa\Desktop\Bob Kerrey-The Band Played Waltzing Matilda.mp3
+print(os.path.basename('E:\music\Various Artists\Bob Kerrey-The Band Played Waltzing Matilda.mp3'))
+#Bob Kerrey-The Band Played Waltzing Matilda.mp3
 print(os.path.getsize('E:\music\Various Artists\Bob Kerrey-The Band Played Waltzing Matilda.mp3')) # 4657964 [bytes]
-print(listdir('E:\music\playlist'))            # ['AaRON - Birds in the Storm.flac', 'AaRON - Inner Streets.flac...
+print(os.listdir('E:\music\playlist'))         # ['AaRON - Birds in the Storm.flac', 'AaRON - Inner Streets.flac...
 #print([f for f in listdir('E:\music\playlist') if isfile(join('E:\music\playlist', f))])
-for data in os.environ:
-    print(os.environ[data])                    # ...
+#for data in os.environ:
+#    print(os.environ[data])                   # ...
 
 import os.path, time
+print("File         : %s" % __file__)          #File: C:\Users\alexa\Desktop\.py\python_tools\import.py
 print("Last modified: %s" % time.ctime(os.path.getmtime("dzagigrow-moon-calendar-2023.pdf")))#Last modified: Mon Jul 31 18:08:48 2023
-print("Created: %s" % time.ctime(os.path.getctime("dzagigrow-moon-calendar-2023.pdf")))      #Created: Mon Jul 31 18:08:48 2023
+print("Created      : %s" % time.ctime(os.path.getctime("dzagigrow-moon-calendar-2023.pdf")))#Created: Mon Jul 31 18:08:48 2023
+print("Access time  : %s" % time.ctime(os.path.getatime("dzagigrow-moon-calendar-2023.pdf")))#Access time: Sat Sep  2 00:42:18 2023
 
 import platform
 print(platform.python_version())               # 3.11.5
