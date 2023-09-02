375a7efb1c4dfd6871542b0cf2c79db30b0d0a86
derarmealex <140951167+derarmealex@users.noreply.github.com>
2023-09-02 15:13:29 +0200

    Update text_formatting.py

--- text_formatting.py
+++ text_formatting.py
@@ -22,9 +22,14 @@ address = "Bangalore, Karnataka, India"                             ##Age: 19
 print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))  ##Address: Bangalore, Karnataka, India
 
 x, y = 4, 3
+
+print("({} + {}) ^ 2 = {}".format(x, y, x * x + 2 * x * y + y * y)) # (4 + 3) ^ 2) = 49
+#or
 final = x * x + 2 * x * y + y * y
 print("({} + {}) ^ 2 = {}".format(x, y, final))                     # (4 + 3) ^ 2) = 49
 #or
-print("({} + {}) ^ 2 = {}".format(x, y, x * x + 2 * x * y + y * y)) # (4 + 3) ^ 2) = 49
+print(f"({x} + {y}) ^ 2 = {final}")                                 # (4 + 3) ^ 2) = 49
 #or
-print('(%d + %d) ^ 2 = %d' % (x, y, final))                         # (4 + 3) ^ 2) = 49
+print("(%d + %d) ^ 2 = %d" % (x, y, final))                         # (4 + 3) ^ 2) = 49
+#or
+print("(%i + %i) ^ 2 = %i" % (x, y, final))                         # (4 + 3) ^ 2) = 49
