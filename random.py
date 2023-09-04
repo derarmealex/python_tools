a2a62c2ed0ec1f8cf2b63e716919637d86dfa903
Alex Green <bartred@seznam.cz>
2023-09-04 18:33:48 +0200

    routine

---
+++ random.py
@@ -0,0 +1,11 @@
+import random
+final = []
+for x in range(0, 10):
+    final.append(random.randint(1, 10))                                           # [5, 6, 2, 3, 3, 10, 10, 9, 1, 7, 8, 7]
+print(final)
+#or
+final = (', '.join(['' + str(random.randint(1, 10)) for x in str(range(0, 10))]))
+print(final)                                                                      # 5, 6, 2, 3, 3, 10, 10, 9, 1, 7, 8, 7
+
+
+input()
