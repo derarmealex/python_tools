9f6d399dcda873c4edb5a67277c075752cdc7ced
Alex Green <bartred@seznam.cz>
2023-09-08 21:40:51 +0200

    routine

--- random_.py
+++ random_.py
@@ -6,6 +6,10 @@ print(final)
 #or
 final = (', '.join(['' + str(random.randint(1, 10)) for x in str(range(0, 10))]))
 print(final)                                                                      # 5, 6, 2, 3, 3, 10, 10, 9, 1, 7, 8, 7
+#or
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
-input()
+random.shuffle(x)
+print(x)                                                                          # 5, 6, 2, 3, 3, 10, 10, 9, 1, 7, 8, 7
+#or
+print(', '.join([str(random.choice(x)) for i in range(0, 10)]))                   # 5, 6, 2, 3, 3, 10, 10, 9, 1, 7, 8, 7
