39b2d74fd85678ad83bac826b8132f753a292731
Alex Green <bartlettgreen@seznam.cz>
2023-09-13 18:31:05 +0200

    routine

--- text_formatting.py
+++ text_formatting.py
@@ -47,11 +47,22 @@ print(f"({x} + {y}) ^ 2 = {final}")                                 # (4 + 3) ^ 
 # CYCLE IN A LINE
 for i in range(0, 10):
     print('*', end="")                                              # **********
-# CYCLE IN A ROW
+# CYCLE IN A ROW WITH LEADING ZEROES
 for i in range(1, 11):                                              #  1: 000000000010
     x = pow(10, i)                                                  ## 2: 000000000100 ...
     print(f'{i:>2}: {x:>012}')                                      ##10: 010000000000
 # or
 x = range(1, 11)                                                    #  1: 000000000010
 for i in x:                                                         ## 2: 000000000100 ...
-    print(f'{i:2d}: {x[-1] ** i:012d}')                             ##10: 010000000000
+    print(f'{i:2}: {x[-1] ** i:012}')                               ##10: 010000000000
+
+stg = '122.22'
+stg = float(stg)
+
+print(f'{stg:>010}')                                                # 0000122.22
+print(f'{stg:010}')                                                 # 0000122.22
+# or
+print('{:>010}'.format(stg))                                        # 0000122.22
+print('{:010}'.format(stg))                                         # 0000122.22
+# or
+print('%010.2f' % stg)                                              # 0000122.22
