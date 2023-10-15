5225c969a730a027e2df7e608d1f6dba61af45b5
Alex Green <alexander.navotnyi@gmail.com>
2023-10-15 15:11:39 +0200

    Update tool_try_except.py

--- tool_try_except.py
+++ tool_try_except.py
@@ -1,3 +1,4 @@
+"""
 try:
     f = open("E:\music\Various Artists\Bob Kerrey-The Band Played Waltzing Matilda.mp3")
 #    f.write("Lorum Ipsum")
@@ -12,3 +13,45 @@ else:
     print("No errors found")
 finally:
     f.close()
+    """
+
+
+# SELF MADE EXCEPTIONS
+class NumbersError(Exception):
+    pass
+
+
+class EvenError(NumbersError):
+    pass
+
+
+class NegativeError(NumbersError):
+    pass
+
+
+def no_even(numbers):
+    if all(x % 2 != 0 for x in numbers):
+        return True
+    raise EvenError("В списке не должно быть чётных чисел")
+
+
+def no_negative(numbers):
+    if all(x >= 0 for x in numbers):
+        return True
+    raise NegativeError("В списке не должно быть отрицательных чисел")
+
+
+def main():
+    print("Введите числа в одну строку через пробел:")
+    try:
+        numbers = [int(x) for x in input().split()]
+        if no_negative(numbers) and no_even(numbers):
+            print(f"Сумма чисел равна: {sum(numbers)}.")
+    except NumbersError as e:  # обращение к исключению как к объекту
+        print(f"Произошла ошибка: {e}.")
+    except Exception as e:
+        print(f"Произошла непредвиденная ошибка: {e}.")
+
+
+if __name__ == "__main__":
+    main()
